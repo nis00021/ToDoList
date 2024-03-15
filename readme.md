aa608433c5d14e206e0e551ada12b7b769e91d8d
nis00021 <126411946+nis00021@users.noreply.github.com>
2024-03-15 22:29:33 +0200

    Lisätty kommentit sekä readme.md tiedosto

---
+++ readme.md
@@ -0,0 +1,9 @@
+To-Do Lista by Niina Järnström
+
+Tämä sovellus on luotu harjoituksena kurssilla Web sovellusten kehittäminen JavaScriptillä.
+
+To-Do Lista on yksinkertainen sovellus joka on rakennettu käyttäen HTML, JavaScript ja CSS tekniikoita.
+Sovelluksen ideana on lisätä listaan tehtäviä, muokata niiden tilaa suorittamatta / suoritettu sekä poistaa tehtäviä kokonaan.
+
+
+
--- script.js
+++ script.js
@@ -1,7 +1,9 @@
+/* Määritetään muuttujat */
 const inputBox = document.getElementById("input-box");
 const listContainer = document.getElementById("list-container");
 const laskuri = document.getElementById("laskuri");
 
+/* Lisätään syöttökentälle toiminnallisuus */
 function addTask() {
   if (inputBox.value === "") {
     alert("Sinun täytyy kirjoittaa jokin tehtävä!");
@@ -20,6 +22,7 @@ function addTask() {
   updateLaskuri();
 }
 
+/* Lisätään tehtävälistalle toimintoja, kuten tehtävän suoritusmerkintä sekä tehtävän poisto */
 listContainer.addEventListener(
   "click",
   function (e) {
@@ -35,17 +38,29 @@ listContainer.addEventListener(
   },
   false
 );
+
+/* Luodaan laskuri joka laskee tehtävien kokonaismäärän sekä suoritetut ja suorittamattomat */
 function updateLaskuri() {
   const tehtävät = document.querySelectorAll("#list-container li").length;
-  const valmiitTehtävät = document.querySelectorAll("#list-container li.valittu").length;
+  const valmiitTehtävät = document.querySelectorAll(
+    "#list-container li.valittu"
+  ).length;
   const tehtävätJäljellä = tehtävät - valmiitTehtävät;
-  laskuri.innerHTML = "Tehtäviä yhteensä " + tehtävät + " <br>Suoritettuja tehtäviä " + valmiitTehtävät + "<br>Tehtäviä suorittamatta " + tehtävätJäljellä;
-
+  laskuri.innerHTML =
+    "Tehtäviä yhteensä " +
+    tehtävät +
+    " <br>Suoritettuja tehtäviä " +
+    valmiitTehtävät +
+    "<br>Tehtäviä suorittamatta " +
+    tehtävätJäljellä;
 }
 
+/*Tällä funktiolla tallennetaan tiedot local storageen */
 function saveData() {
   localStorage.setItem("data", listContainer.innerHTML);
 }
+
+/* Tällä funktiolla haetaan local storageen tallennetut tiedot*/
 function showTask() {
   listContainer.innerHTML = localStorage.getItem("data");
   updateLaskuri();
